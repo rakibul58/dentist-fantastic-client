5ac5f97197a14ab02484c80179bec5a710557a85
rakibul58 <rhrahi14@gmail.com>
2022-11-10 09:02:53 +0600

    sorting done after all

--- src/components/ServiceDetails/ServicesDetails.js
+++ src/components/ServiceDetails/ServicesDetails.js
@@ -30,7 +30,8 @@ const ServicesDetails = () => {
             photoURL: user?.photoURL,
             service,
             review,
-            time
+            time,
+            sort_id: oldComment.length
         }
 
         fetch('http://localhost:5000/comments', {
@@ -50,10 +51,6 @@ const ServicesDetails = () => {
                     // const sortedDates = newComment?.map(obj => { return { ...obj, time: new Date(obj.time) } })
                     //     .sort((a, b) => b.time - a.time)
                     // console.log(sortedDates);
-                    newComment.sort(function(a, b) {
-                        return (a._id - b._id);
-                    });
-                    newComment.reverse();
                     setOldComment(newComment);
 
                 }
@@ -61,12 +58,12 @@ const ServicesDetails = () => {
             .catch(err => console.error(err));
     }
 
+
     useEffect(() => {
         fetch(`http://localhost:5000/comments/${_id}`)
             .then(res => res.json())
             .then(data => setOldComment(data));
-    }, [_id]);
-
+    }, [_id , oldComment]);
 
 
     // console.log(oldComment)
